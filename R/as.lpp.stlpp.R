adfc386e94aff6bebf0e21880440908c5d9c25fa
Andrea Gilardi <a.gilardi5@campus.unimib.it>
2020-12-18 22:26:53 +0100

    add arg to as.lpp.stlpp

--- R/as.lpp.stlpp.R
+++ R/as.lpp.stlpp.R
@@ -1,6 +1,6 @@
 #' @export
-as.lpp.stlpp <- function(x,...){
+as.lpp.stlpp <- function(x, ..., linnet_sparse = FALSE){
   if(!any(class(x)=="stlpp")) stop("class(x) must be stlpp")
-  Y <- as.lpp(x=x$data$x,y=x$data$y,L=x$domain,sparse = TRUE,...)
+  Y <- as.lpp(x=x$data$x,y=x$data$y,L=x$domain,sparse = linnet_sparse,...)
   return(Y)
 }
